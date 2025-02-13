2a31b8fbfc20aa6e32787d93a02f307f0f34358f
kevin-ramen <kevin3cmpunk@hotmail.com>
2025-02-13 17:58:27 -0600

    Initial Commit

---
+++ Scenes/Menu/game_modes.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+func _on_crazy_tiles_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu/crazy_tiles.tscn")
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
