035386c911b4b18092fd003a28b7225d59d1ecd1
kevin-ramen <kevin3cmpunk@hotmail.com>
2025-02-13 20:40:35 -0600

    Create tile-platform

--- Scenes/Menu/game_modes.gd
+++ Scenes/Menu/game_modes.gd
@@ -6,3 +6,7 @@ func _on_crazy_tiles_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
+
+
+func _on_single_player_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world/world.tscn") # Replace with function body.
